6859f3f9910d014427dd639b99385af7f3fdbe93
Pablo Lagos <pablolagos@exploratodo.com>
2020-04-22 10:51:06 -0400

    Json functions

---
+++ json/json.go
@@ -0,0 +1,41 @@
+package json
+
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"os"
+)
+
+func loadJsonFomFile(filename string) (result interface{}, err error) {
+	/* Leer rawdata */
+	rawJsonData, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return result, err
+	}
+
+	/* Validaciones */
+	if !json.Valid(rawJsonData) {
+		return result, errors.New("No valid json data")
+	}
+
+	/* Obtener datos */
+	err = json.Unmarshal(rawJsonData, &result)
+	return result, err
+}
+
+func SaveJson(object interface{}, filename string, perm os.FileMode) error {
+
+	/* Convertir a texto */
+	rawdata, err := json.Marshal(object)
+	if err != nil {
+		return err
+	}
+
+	/* Write to file */
+	err = ioutil.WriteFile(filename, rawdata, perm)
+	if err != nil {
+		return nil
+	}
+	return nil
+}
